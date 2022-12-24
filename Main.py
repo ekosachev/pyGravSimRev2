14a9b24d6e6313ad721add96b1925bb3b7b0ef0c
Vinermy <egorkosachev@vk.com>
2022-12-24 19:31:16 +0700

    Added comments to the code

--- Main.py
+++ Main.py
@@ -1,20 +1,18 @@
 import sys
 import time
+from threading import Thread, Condition
 from typing import List
-from json import load
 
 import PySide6.QtCore as QtCore
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
+
 from MainwindowUi import MainwindowUi
-from Particle import Particle
-from Simulation import Simulation
-from Vector2D import Vector2D
-from threading import Thread, Condition
 from SaveLoad import *
+from Simulation import Simulation
 
 
-class ListItem(object):
+class ListItem(object): # Helps to associate captions in the lstBodies to the Particle class objects
     def __init__(self, particle: Particle):
         self.particle = particle
         self.label = repr(particle)
@@ -22,24 +20,24 @@ class ListItem(object):
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super(MainWindow, self).__init__() # Load the interface from MainwindowUi file
         self.simulation_cycle_thread = None
         self.draw_cycle_thread = None
         self.simulation_instance = None
         self.ui = MainwindowUi()
         self.ui.setupUi(self)
-        self.current_min_free_id: int = 1
+        self.current_min_free_id: int = 1 # Minimal unoccupied particle id
         self.particle_list: List[ListItem] = []
         self.draw_condition = Condition()
 
 
         self.simulation_running = False
-        with open("config.json") as f:
+        with open("config.json") as f: # Load the config file
             self.config = load(f)
 
-        self.frame_delay: float = 1 / self.config["SIMULATION"]["fps_limit"]
+        self.frame_delay: float = 1 / self.config["SIMULATION"]["fps_limit"] # Set the minimal delay between frames
 
-        # CONNECTIONS
+        # Connect the functions to the corresponding buttons
         self.ui.btnAdd.clicked.connect(self.add_particle)
         self.ui.btnDelete.clicked.connect(self.remove_particle)
         self.ui.btnSimStart.clicked.connect(self.start_simulation)
@@ -50,16 +48,17 @@ class MainWindow(QMainWindow):
         self.ui.btnLoad.clicked.connect(self.load_state_from_file)
         self.ui.btnEdit.clicked.connect(self.edit_particle)
 
+        # Disable the buttons to prevent exceptions
         self.ui.btnShowHeatMap.setDisabled(True)
         self.ui.btnSimStop.setDisabled(True)
         self.ui.cbbColorDependsOn.setDisabled(True)
 
-    def closeEvent(self, *args, **kwargs):
+    def closeEvent(self, *args, **kwargs): # Ensure that additional threads are stopped whe the app is exited
         super(QMainWindow, self).closeEvent(*args, **kwargs)
         if self.simulation_running:
             self.stop_simulation()
 
-    def update_list_view(self):
+    def update_list_view(self): # Manually update the lstBodies to reflect any changes
         self.ui.lstBodies.clear()
         self.ui.lstBodies.addItems([x.label for x in self.particle_list])
 
@@ -82,7 +81,7 @@ class MainWindow(QMainWindow):
                 break
         self.update_list_view()
 
-    def edit_particle(self):
+    def edit_particle(self): # Replace the selected lstBodies item with another one that has new parameters
         selected_item = self.ui.lstBodies.selectedItems()[0].text()
         for i in range(len(self.particle_list)):
             if self.particle_list[i].label == selected_item:
@@ -102,9 +101,9 @@ class MainWindow(QMainWindow):
                                                   filter="JSON files (*.json)",
                                                   dir="./saves/")
         new_particles = load_initial_state(filename)
-        self.particle_list = []
+        self.particle_list = [] # Delete all particles
         for particle in new_particles:
-            self.particle_list.append(ListItem(particle))
+            self.particle_list.append(ListItem(particle)) # Load new ones from the savefile
         self.update_list_view()
 
     def save_state_to_file(self):
@@ -116,23 +115,23 @@ class MainWindow(QMainWindow):
     def start_simulation(self):
         self.simulation_instance = Simulation(particles=[x.particle for x in self.particle_list],
                                               framesize=(int(self.config["SIMULATION"]["frame_size_x"]),
-                                                         int(self.config["SIMULATION"]["frame_size_y"])))
+                                                         int(self.config["SIMULATION"]["frame_size_y"]))) # Create a new simulation instance
         self.simulation_running = True
         self.draw_cycle_thread = Thread(target=self.draw_cycle)
         self.simulation_cycle_thread = Thread(target=self.simulation_cycle)
-        self.simulation_cycle_thread.start()
+        self.simulation_cycle_thread.start() # Start additional threads
         self.draw_cycle_thread.start()
-        self.ui.btnShowHeatMap.setDisabled(True)
+        self.ui.btnShowHeatMap.setDisabled(True) # Disable buttons to prevent exceptions
         self.ui.btnSimStart.setDisabled(True)
         self.ui.btnSimStep.setDisabled(True)
         self.ui.gpbEdit.setDisabled(True)
-        self.ui.btnSimStop.setEnabled(True)
+        self.ui.btnSimStop.setEnabled(True) # Enable the buttons that stops the simulation
 
     def stop_simulation(self):
         self.simulation_running = False
-        self.simulation_cycle_thread.join()
+        self.simulation_cycle_thread.join() # Join the threads
         self.draw_cycle_thread.join()
-        self.ui.btnShowHeatMap.setEnabled(True)
+        self.ui.btnShowHeatMap.setEnabled(True) # Enable buttons
         self.ui.btnSimStart.setEnabled(True)
         self.ui.btnSimStep.setEnabled(True)
         self.ui.gpbEdit.setEnabled(True)
@@ -141,10 +140,10 @@ class MainWindow(QMainWindow):
     def draw_cycle(self):
         while self.simulation_running:
             with self.draw_condition:
-                self.draw_condition.notify()
-                time.sleep(self.frame_delay)
+                self.draw_condition.notify() # Allow the new step to be rendered
+                time.sleep(self.frame_delay) # Wait the minimal delay
 
-    def get_dependent_coloring_type(self) -> str:
+    def get_dependent_coloring_type(self) -> str: # Convert the selected text to pre-defined values
         match self.ui.cbbColorDependsOn.currentText():
             case "скорости":
                 return "velocity"
@@ -154,9 +153,9 @@ class MainWindow(QMainWindow):
     def simulation_cycle(self):
         while self.simulation_running:
             with self.draw_condition:
-                self.draw_condition.wait(1.0)
+                self.draw_condition.wait(1.0) # Wait until the permission from draw_cycle, but no more than 1 second
                 self.ui.lblSimulationDisplay.setPixmap(QPixmap.fromImage(
-                    self.simulation_instance.run_step(
+                    self.simulation_instance.run_step( # Generate the new frame
                         draw_barycenter=self.ui.cbxDrawMassCenter.isChecked(),
                         draw_velocity_vectors=self.ui.cbxDrawSpdVects.isChecked(),
                         draw_trails=self.ui.cbxDrawTrails.isChecked(),
@@ -164,10 +163,10 @@ class MainWindow(QMainWindow):
                         dependent_coloring_type=self.get_dependent_coloring_type())).scaled(
                     self.ui.lblSimulationDisplay.width(),
                     self.ui.lblSimulationDisplay.height(),
-                    QtCore.Qt.KeepAspectRatio
+                    QtCore.Qt.KeepAspectRatio # Resize it so it fits the size of lblSimulationDisplay
                 ))
 
-    def draw_heatmap(self):
+    def draw_heatmap(self): # Generate the force heatmap
         self.ui.lblSimulationDisplay.setPixmap(QPixmap.fromImage(
             self.simulation_instance.draw_force_heatmap().scaled(
                 self.ui.lblSimulationDisplay.width(),
@@ -175,7 +174,7 @@ class MainWindow(QMainWindow):
                 QtCore.Qt.KeepAspectRatio
             )))
 
-    def run_single_step(self):
+    def run_single_step(self): # Generates the next frame of current simulation
         self.ui.lblSimulationDisplay.setPixmap(QPixmap.fromImage(
             self.simulation_instance.run_step(
                 draw_barycenter=self.ui.cbxDrawMassCenter.isChecked(),
--- Simulation.py
+++ Simulation.py
@@ -9,7 +9,7 @@ from PIL.ImageQt import ImageQt
 
 class Simulation(object):
     def __init__(self, particles: List[Particle], framesize: tuple[int, int]) -> None:
-        with open('config.json') as config:
+        with open('config.json') as config: # Load the config file
             self.config = load(config)
 
         self.frameCounter = 0
@@ -34,26 +34,26 @@ class Simulation(object):
                  dependent_coloring_type: str) -> ImageQt:
 
         # Calculate accelerations for all particles
-        self.velocityLog.append([])
+        self.velocityLog.append([]) # Add new rows for the data to be saved to
         self.accelerationLog.append([])
-        for attracted in self.particles:
-            for attractor in self.particles:
-                distance: Vector2D = attractor.position - attracted.position
+        for attracted in self.particles: # For every particle in the list...
+            for attractor in self.particles: # For each other particle...
+                distance: Vector2D = attractor.position - attracted.position # Get the distance between particles
                 try:
-                    acceleration = distance.stretch(attractor.mass / pow(distance.len(), 2))
+                    acceleration = distance.stretch(attractor.mass / pow(distance.len(), 2)) # Calculate the acceleration
                     attracted.velocity += acceleration
                     attracted.acceleration = acceleration
                 except ZeroDivisionError:
                     continue
-            self.maxVelocity = max(self.maxVelocity, attracted.velocity.len())
+            self.maxVelocity = max(self.maxVelocity, attracted.velocity.len()) # Update min/max values
             self.minVelocity = min(self.minVelocity, attracted.velocity.len())
             self.maxAcceleration = max(self.maxAcceleration, attracted.acceleration.len())
             self.minAcceleration = min(self.minAcceleration, attracted.acceleration.len())
-            self.velocityLog[-1].append(attracted.velocity.len())
+            self.velocityLog[-1].append(attracted.velocity.len()) # Save the info about current step
             self.accelerationLog[-1].append(attracted.acceleration.len())
-            self.calculatedPositions.append(attracted.position + attracted.velocity)
+            self.calculatedPositions.append(attracted.position + attracted.velocity) # Save the calculated position
 
-        for i in range(len(self.particles)):
+        for i in range(len(self.particles)): # Move particles
             self.particles[i].position = self.calculatedPositions[i]
         self.positionLog.append(self.calculatedPositions)
         self.calculatedPositions = []
@@ -63,15 +63,15 @@ class Simulation(object):
                                  tuple(self.config["DRAWING"]["background_color"]))  # Clear the frame
 
         if draw_trails:
-            for i in range(len(self.positionLog)):
+            for i in range(len(self.positionLog)): # For each body...
                 if i == 0:
                     continue
-                for j in range(len(self.positionLog[i])):
+                for j in range(len(self.positionLog[i])): # Draw the line between it's position on every consecutive frames
                     self.frameDraw.line(xy=((self.positionLog[i][j].x + self.framesize[0] // 2,
                                              self.positionLog[i][j].y + self.framesize[0] // 2),
                                             (self.positionLog[i - 1][j].x + self.framesize[0] // 2,
                                              self.positionLog[i - 1][j].y + self.framesize[0] // 2)),
-                                        fill=tuple(map(lambda x: min(int(x * pow(self.config["DRAWING"]["trails_fade"],
+                                        fill=tuple(map(lambda x: min(int(x * pow(self.config["DRAWING"]["trails_fade"], # Make the trail fade over time
                                                                                  len(self.positionLog) - i)), 255),
                                                        self.config["DRAWING"]["trail_color"])))
 
@@ -79,14 +79,14 @@ class Simulation(object):
             if dependent_coloring:
                 match dependent_coloring_type:
                     case "velocity":
-                        interpolated_velocity = translate(particle.velocity.len(), self.minVelocity, self.maxVelocity)
+                        interpolated_velocity = translate(particle.velocity.len(), self.minVelocity, self.maxVelocity) # Interpolate the velocity to 0-1 range
                         c1 = self.config["DRAWING"]["velocity_gradient_color_0"]
                         c2 = self.config["DRAWING"]["velocity_gradient_color_1"]
                         color = [0, 0, 0]
                         for comp in range(3):
-                            color[comp] = int(c1[comp] + interpolated_velocity * (c2[comp] - c1[comp]))
+                            color[comp] = int(c1[comp] + interpolated_velocity * (c2[comp] - c1[comp])) # Interpolate the velocity between every component of c1 and c2
                     case "acceleration":
-                        interpolated_acceleration = translate(particle.acceleration.len(),
+                        interpolated_acceleration = translate(particle.acceleration.len(), # The same procedure, but with the acceleration
                                                               self.minAcceleration,
                                                               self.maxAcceleration)
                         c1 = self.config["DRAWING"]["acceleration_gradient_color_0"]
@@ -95,41 +95,41 @@ class Simulation(object):
                         for comp in range(3):
                             color[comp] = int(c1[comp] + interpolated_acceleration * (c2[comp] - c1[comp]))
                     case _:
-                        color = self.config["DRAWING"]["particle_color"]
+                        color = self.config["DRAWING"]["particle_color"] # In case dependent_coloring_type does not fall into one of the cases just use the default color
             else:
                 color = self.config["DRAWING"]["particle_color"]
 
-            pos: Vector2D = Vector2D(self.framesize[0] // 2, self.framesize[1] // 2) + particle.position
-            self.frameDraw.ellipse(xy=((pos.x - particle.radius,
+            pos: Vector2D = Vector2D(self.framesize[0] // 2, self.framesize[1] // 2) + particle.position # Translate the particle coordinates to the frame's coordinate system
+            self.frameDraw.ellipse(xy=((pos.x - particle.radius, # Draw the particle
                                         pos.y - particle.radius),
                                        (pos.x + particle.radius,
                                         pos.y + particle.radius)),
                                    fill=tuple(color))
 
-            self.frameDraw.text((pos.x,
+            self.frameDraw.text((pos.x, # Render it's id
                                  pos.y),
                                 str(particle.id),
                                 fill=tuple(self.config["DRAWING"]["particle_label_color"]))
 
             if draw_velocity_vectors:
-                self.frameDraw.line(xy=((pos.x, pos.y),
-                                        (pos.x + particle.velocity.x * (self.config["DRAWING"]["vel_vect_multiplier"]),
+                self.frameDraw.line(xy=((pos.x, pos.y), # Draw the line between particle's position on this frame and the next frame assuming that velocity does not change
+                                        (pos.x + particle.velocity.x * (self.config["DRAWING"]["vel_vect_multiplier"]), # multiply the velocity, so the line is easier to be seen
                                          pos.y + particle.velocity.y * (
                                              self.config["DRAWING"]["vel_vect_multiplier"]))),
                                     fill=tuple(self.config["DRAWING"]["velocity_vectors_color"]))
 
             if draw_barycenter:
-                barycenter_position: Vector2D = sum([particle.position * particle.mass for particle in self.particles],
+                barycenter_position: Vector2D = sum([particle.position * particle.mass for particle in self.particles], #Find the average position weighted by mass
                                                     Vector2D(0, 0)) / sum(
                     [particle.mass for particle in self.particles])
-                barycenter_position = Vector2D(self.framesize[0] // 2, self.framesize[1] // 2) + barycenter_position
+                barycenter_position = Vector2D(self.framesize[0] // 2, self.framesize[1] // 2) + barycenter_position # Translate to frame's coordinate system
                 self.frameDraw.ellipse(xy=(barycenter_position.x - 5,
                                            barycenter_position.y - 5,
                                            barycenter_position.x + 5,
                                            barycenter_position.y + 5),
-                                       fill=tuple(self.config["DRAWING"]["barycenter_color"]))
+                                       fill=tuple(self.config["DRAWING"]["barycenter_color"])) # Draw the barycenter at that coordinates
 
-        return ImageQt(self.frameImage)
+        return ImageQt(self.frameImage) # Return the frame image
 
     def draw_force_heatmap(self) -> ImageQt:
         # Calculate the force applied to every pixel of the frame
